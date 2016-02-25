647fd29d80dd343ec332e60ee24bb816789f6420
tjelsa <tjelsat@gmail.com>
2016-02-25 21:09:55 +0100

    Update models.py

--- brus/liste/models.py
+++ brus/liste/models.py
@@ -2,23 +2,22 @@ from django.db import models
 
 
 class Brus(models.Model):
-    name = models.CharField(max_length = 50)
+    soda = models.CharField(max_length=20)
     cost = models.IntegerField(default=15)
 
     def __str__(self):
         return '%s %s' % (self.name, self.money)
 
-class  Person(models.Model):
+
+class Person(models.Model):
     name = models.CharField(max_length=50)
     balance = models.IntegerField(default=0)
 
-
-    def add_money(self, amount):
+    def deposit_money(self, amount):
         self.money += amount
         self.save()
 
-    def remove_money(self, amount):
+    def withdraw_money(self, amount):
         self.money -= amount
         self.save()
-
 
