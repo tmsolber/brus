98919e7f92e31804b66ce546479fddb377e38021
Tiril Merethe Solberg <tiril.m.solberg@gmail.com>
2016-04-28 21:18:05 +0200

    Reverses ordering of persons and adds max-value cap.

--- brus/liste/forms.py
+++ brus/liste/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class DepositForm(forms.Form):
-    deposit_amount = forms.IntegerField(label='kr', min_value=0, widget=forms.NumberInput(attrs={'class': 'form-control',
+    deposit_amount = forms.IntegerField(label='kr', min_value=0, max_value=2000, widget=forms.NumberInput(attrs={'class': 'form-control',
                                                                                                  'placeholder' : 'kr.'
                                                                                                  }))
 
